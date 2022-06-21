550e51ccca866d20e3ff8a0e8a57f193745aa983
kaheyio <gricad@hotmail.com>
2022-06-21 11:31:11 +0200

    commited lucaaas prck chui aveugle

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,4 @@ const authRoute = require('./routes/auth');
 app.use('/api/user', authRoute);
 
 
-app.listen(3000, () => console.log('Server works'));
+app.listen(3000, () => console.log('Server works LUCAAAAAAAAAS'));
