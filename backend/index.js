e195429eb156cf101736dd83e531bdec3d772772
LDEV0422 <lsreg-pro@outlook.com>
2022-06-21 13:23:54 +0200

    authentication

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 // Import Routes
 const authRoute = require('./routes/auth');
+
+// to hide database credentials
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true },() => 
+console.log('Connected to database')
+);
+
+// Post request Middleware (we use express's body parser so we can send post requests)
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
 
 // Routes Middlewares
 app.use('/api/user', authRoute);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,55 @@
 const router = require('express').Router();
+const Users = require('../db_model/Users');
 
 // NB: full route is localhost:PORT/api/user/
 router.get('/', async (req, res) => {
-    res.send('Test route, it works !');
+    const users = await Users.find();
+    res.send(users);
 });
+
 
 // NB full route is localhost:PORT/api/user/register
 router.post('/register', async (req, res) => {
-    await res.send('Register !');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // check if user already exists
+    const findUser = await Users.findOne({
+        email
+    });
+    if (findUser != null) {
+        res.send('This user already exists');
+        return;
+    }
+
+    const user = new Users({
+        username,
+        email,
+        password
+    });
+
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
 
+    try {
+        const deleteUser = await Users.deleteOne({
+            _id: id
+        });
+        res.json(deleteUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+
+});
 
 module.exports = router;
