cf789cff6a1fee8e05ee12fef4de972c02f3e40d
kaheyio <gricad@hotmail.com>
2022-06-24 11:40:47 +0200

    lulu, created components file structure

--- angu8leika/src/app/app.module.ts
+++ angu8leika/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoggedComponent } from './components/logged/logged.component';
+import { TransferComponent } from './components/logged/transfer/transfer.component';
+import { AccountsComponent } from './components/logged/accounts/accounts.component';
+import { ContactComponent } from './components/logged/contact/contact.component';
+import { TransactionsComponent } from './components/logged/accounts/transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoggedComponent,
+    TransferComponent,
+    AccountsComponent,
+    ContactComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
