82ccc90dff58915a45f2a7642bafbe5ec72f5b92
LDEV0422 <lsreg-pro@outlook.com>
2022-06-26 19:35:42 +0200

    change path for login comp in auth module

--- angu8leika/src/app/modules/auth/auth.module.ts
+++ angu8leika/src/app/modules/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '../components/login/login.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from 'src/app/components/login/login.component';
 
 
 
