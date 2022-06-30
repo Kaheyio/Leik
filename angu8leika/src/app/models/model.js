cda4652e454de26163343e5434bea5974d725644
kaheyio <gricad@hotmail.com>
2022-06-30 12:26:49 +0200

    model

---
+++ angu8leika/src/app/models/model.js
@@ -0,0 +1,22 @@
+
+"OPERATION TEMPLATE"
+operationtitle = ""
+emissiondate = ""
+type = "card or prelevement or newmove"
+targetaccount = ""
+target = "cardnumber or prelevref or newmovenote"
+amount = "+ or -"
+status = "pending or incoming or history or rejected" //luluhistory = validated
+
+"PENDING"
+leikodevalidationstatus = "pending refused confirmed"; //action de l'utilisateur
+bankvalidationstatus = "true or false" //nous on check le montant
+
+"REJECTED"
+rejectionmotif = "leikoderefused bankrefused" //ce qu'on affiche danse
+
+"INCOMING" //seulement prelevref et newmoveid
+estimateddate = "date"
+
+"HISTORY"
+// C'est bon on a tout frate
