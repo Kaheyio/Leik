2f505a9a624729303fc00f444f061b7c229fe582
kaheyio <gricad@hotmail.com>
2022-06-30 12:45:10 +0200

    moved content from app to main

--- angu8leika/src/app/components/main/main.component.ts
+++ angu8leika/src/app/components/main/main.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  login = false;
+  logged = false;
+
   constructor() { }
 
   ngOnInit(): void {
